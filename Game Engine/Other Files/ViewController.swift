393eb5e9ad5279b7eb2bc42d03ebce6bc2c1e67d
Tom Beardsley <t.beardsley@hotmail.co.uk>
2025-03-08 14:46:24 -0500

    2. render a green screen

---
+++ Game Engine/Other Files/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Game Engine
+//
+//  Created by Tom B on 2025-03-08.
+//
+
+import Cocoa
+
+class ViewsController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Game EngineTests/Game_EngineTests.swift
@@ -0,0 +1,17 @@
+//
+//  Game_EngineTests.swift
+//  Game EngineTests
+//
+//  Created by Tom B on 2025-03-08.
+//
+
+import Testing
+@testable import Game_Engine
+
+struct Game_EngineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
