db4114e5b12acd9c30bcaeef25bf65e6470c50bd
Tom Beardsley <t.beardsley@hotmail.co.uk>
2025-03-11 10:04:29 -0400

    7.Completed adding Renderer and GameObject classes. Gameview delegates rendering to the Renderer class.

--- Game Engine/Display/GameView.swift
+++ Game Engine/Display/GameView.swift
@@ -4,13 +4,7 @@ import Cocoa
 
 class GameView: MTKView {
     
-    // Array of vertices that is populated by function: createVertices()
-    var vertices: [Vertex]!
-    
-    // Create gpu accessible memory
-    var vertexBuffer: MTLBuffer!
-    
-    
+    var renderer: Renderer!         // needs Engine.Ignite(device: self.device!) to fire before it can be instantiated
     
     // Constructor
     required init(coder: NSCoder) {
@@ -28,55 +22,16 @@ class GameView: MTKView {
         // Has to match the format of the fragment shader
         self.colorPixelFormat = Preferences.MainPixelFormat
         
-        // Create vertex data
-        createVertices()
+        // instantiate a GameObject
+        self.renderer = Renderer()
         
-        // Create buffers of gpu accessible memory
-        createBuffers()
+        // All draw functionality of this (self) GameView:MTKView object will be delegated to a Renderer class object.
+        self.delegate = self.renderer
     }
     
     
-    func createVertices() {
-        self.vertices = [
-            Vertex(position: SIMD3<Float>(0, 1, 0), colour: SIMD4<Float>(1, 0, 0, 1)),
-            Vertex(position: SIMD3<Float>(-1, -1, 0), colour: SIMD4<Float>(0, 1, 0, 1)),
-            Vertex(position: SIMD3<Float>(1, -1, 0), colour: SIMD4<Float>(0, 0, 1, 1))
-        ]
-    }
+    // Mouse Input
     
-    
-    func createBuffers() {
-        self.vertexBuffer = self.device?.makeBuffer(bytes: self.vertices, length: Vertex.stride(self.vertices.count), options: [])
-    }
-    
-    
-    // Override the draw function
-    override func draw(_ dirtyRect: NSRect) {
-        
-        // Try and grab the drawable and renderPassDecriptor.
-        guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
-        
-        // Create a command buffer that will go into the command queue
-        let commandBuffer = Engine.CommandQueue.makeCommandBuffer()
-        
-        // Create a render command encoder for the command buffer we just created.
-        // renderPassDescriptor contains a lot of pixel information and buffer storage information for our view for the next drawable.
-        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        
-        // Set the render pipeline state of the render command encoder.
-        renderCommandEncoder?.setRenderPipelineState(RenderPipelineStateLibrary.PipelineState(.Basic))
-        
-        // Set the location of the data in device space. Specify that a triangle primitive should be drawn for every 3 vertices
-        renderCommandEncoder?.setVertexBuffer(self.vertexBuffer, offset: 0, index: 0)
-        renderCommandEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: self.vertices.count)
-        
-        renderCommandEncoder?.endEncoding()
-        
-        // Present the next drawable to the screen.
-        commandBuffer?.present(drawable)
-        
-        // Commit the drawable once it's ready
-        commandBuffer?.commit()
-    }
+    // Keyboard Input
 
 }
---
+++ Game Engine/Display/Renderer.swift
@@ -0,0 +1,42 @@
+import MetalKit
+
+class Renderer: NSObject {
+    
+    var gameObject: GameObject = GameObject()
+    
+}
+
+
+// MTKViewDelegate is a protocol that allows you to receive updates from an MTKView (Metal View).
+// Allows handling of two key events: (1) Resizing the view, (2) Drawing the view.
+extension Renderer: MTKViewDelegate {
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        // Fires when the window is resized (has be be overridden)
+    }
+    
+    func draw(in view: MTKView) {
+        // Will be passing in the view we are delegating from. draw() will be called automatically.
+        
+        // Try and grab the drawable and renderPassDecriptor.
+        guard let drawable = view.currentDrawable, let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+        
+        // Create a command buffer that will go into the command queue
+        let commandBuffer = Engine.CommandQueue.makeCommandBuffer()
+        
+        // Create a render command encoder for the command buffer we just created.
+        // renderPassDescriptor contains a lot of pixel information and buffer storage information for our view for the next drawable.
+        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        
+        // Render the game object
+        self.gameObject.render(renderCommandEncoder: renderCommandEncoder!)
+        
+        renderCommandEncoder?.endEncoding()
+        
+        // Present the next drawable to the screen.
+        commandBuffer?.present(drawable)
+        
+        // Commit the drawable once it's ready
+        commandBuffer?.commit()
+    }
+}
